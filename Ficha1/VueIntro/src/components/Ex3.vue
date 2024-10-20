dfe1c7194e5f90dc9f975dbda4ce985a83478834
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2024-10-20 22:51:38 +0100

    Add files via upload

---
+++ Ficha1/VueIntro/src/components/Ex3.vue
@@ -0,0 +1,32 @@
+<template>
+    <h2>Peso: {{weight}} kgs</h2>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        weight: 72
+      };
+    },
+    beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  };
+  </script>
+  
---
+++ Ficha1/VueIntro/src/components/Ex4.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h1>{{ productName }}</h1>
+      <p v-html="productDesc"></p>
+      <img :src="productImage" alt="Product Image" />
+      <p v-if="productPrice < 1000">{{ productPrice }}€</p>
+      <button @click="resetPrice">Reset Price</button>
+      <p>{{ productAvailable ? 'DISPONÍVEL' : 'NÃO DISPONÍVEL' }}</p>
+      <button @click="buyProduct">Comprar</button>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        productName: "Dell XPS",
+        productDesc: "Processador i7<br>Disco SSD 256GB<br> Monitor Samsung 21'",
+        productImage: "https://tinyurl.com/2p9dnx68",
+        productPrice: 1200,
+        productAvailable: true
+      };
+    },
+    methods: {
+      resetPrice() {
+        this.productPrice = 1200;
+      },
+      buyProduct(){
+        this.productAvailable = false;
+      }
+    }
+  };
+</script>
+  
